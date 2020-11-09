9d8811e4221f31e7276f16f79fafac70aaafe03f
Nakagawakeita <nakagawakeita10@gmail.com>
2020-11-10 08:34:42 +0900

    第６回終了

--- Assets/Scripts/DestroyObject.cs
+++ Assets/Scripts/DestroyObject.cs
@@ -7,6 +7,14 @@ using UnityEngine;
 /// </summary>
 public class DestroyObject : MonoBehaviour
 {
+    // エフェクトプレハブのデータを入れるための箱を作る。
+    [SerializeField]
+    private GameObject effectPrefab;
+
+    [SerializeField]
+    private GameObject effectPrefab2; // 2種類目のエフェクトを入れるための箱
+    public int objectHP;
+
     // このメソッドはぶつかった瞬間に呼び出される
     //Collider型のオブジェクトに衝突が検出されると呼び出される
     private void OnTriggerEnter(Collider other)
@@ -15,14 +23,35 @@ public class DestroyObject : MonoBehaviour
         //もし衝突した相手オブジェクトのTagにShellという名前がある場合
         if (other.CompareTag("Shell"))
         {
-            // このスクリプトがついているオブジェクトを破壊する（thisは省略が可能）
-            //このゲームオブジェクト(この場合Block)を破棄する
-            Destroy(this.gameObject);
+            // オブジェクトのHPを１ずつ減少させる。
+            objectHP -= 1;
 
-            // ぶつかってきたオブジェクトを破壊する
-            // otherがどこと繋がっているか考えてみよう！
-            //ゲームオブジェクト(この場合ShotShell)を破棄する
-            Destroy(other.gameObject);
+            // もしもHPが0よりも大きい場合には（条件）
+            if (objectHP > 0)
+            {
+
+                // このスクリプトがついているオブジェクトを破壊する（thisは省略が可能）
+                //このゲームオブジェクト(この場合Block)を破棄する
+                Destroy(other.gameObject);
+
+                // エフェクトを実体化（インスタンス化）する
+                GameObject effect = Instantiate(effectPrefab, other.transform.position, Quaternion.identity);
+                // エフェクトを２秒後に画面から消す
+                Destroy(effect, 2.0f);
+            }
+            else
+            {
+                Destroy(other.gameObject);
+
+                // もう１種類のエフェクを発生させる。
+                GameObject effect2 = Instantiate(effectPrefab2, other.transform.position, Quaternion.identity);
+                Destroy(effect2, 2.0f);
+
+                // ぶつかってきたオブジェクトを破壊する
+                // otherがどこと繋がっているか考えてみよう！
+                //ゲームオブジェクト(この場合ShotShell)を破棄する
+                Destroy(this.gameObject);
+            }
         }
     }
 }

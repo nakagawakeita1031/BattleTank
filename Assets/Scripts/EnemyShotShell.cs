db04d55e221b623c18b1f81a83c8eb86dfbede53
Nakagawakeita <nakagawakeita10@gmail.com>
2020-11-10 09:30:22 +0900

    第７回終了

---
+++ Assets/Scripts/EnemyShotShell.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 敵の砲弾
+/// </summary>
+public class EnemyShotShell : MonoBehaviour
+{
+    public float shotSpeed;
+    [SerializeField]
+    private GameObject enemyShellPrefab;
+    [SerializeField]
+    private AudioClip shotSound;
+    private int interval;
+
+    void Update()
+    {
+        interval += 1;
+
+        if (interval % 60 == 0)
+        {
+            GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
+
+            Rigidbody enemyShellRb = enemyShell.GetComponent<Rigidbody>();
+
+            // forwardはZ軸方向（青軸方向）・・・＞この方向に力を加える。
+            enemyShellRb.AddForce(transform.forward * shotSpeed);
+
+            AudioSource.PlayClipAtPoint(shotSound, transform.position);
+
+            Destroy(enemyShell, 3.0f);
+        }
+    }
+}

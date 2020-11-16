c8b6eaf095dafd059cfc93d54ffc097d0cdc0248
Nakagawakeita <nakagawakeita10@gmail.com>
2020-11-17 04:18:29 +0900

    第16回終了

---
+++ Assets/Scripts/GameStart.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameStart : MonoBehaviour
+{
+    // 先頭に「public」が付いていることを確認する（ポイント）
+    public void OnStartButtonClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}

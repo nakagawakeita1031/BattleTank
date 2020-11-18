16cbbb0526a1356d043fa46f511b582e05af06c6
Nakagawakeita <nakagawakeita10@gmail.com>
2020-11-19 04:42:39 +0900

    第24回終了

--- Assets/Scripts/EnemyShotShell.cs
+++ Assets/Scripts/EnemyShotShell.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 /// <summary>
 /// 敵の砲弾
@@ -14,11 +15,26 @@ public class EnemyShotShell : MonoBehaviour
     private AudioClip shotSound;
     private int interval;
 
+    // 何秒間停止させるかは自由
+    public float stopTimer = 5.0f;
+
+    [SerializeField]
+    private Text stopLabel;
+
     void Update()
     {
         interval += 1;
 
-        if (interval % 60 == 0)
+        stopTimer -= Time.deltaTime;
+        // タイマーが0未満になったら、0で止める。
+        if (stopTimer < 0)
+        {
+            stopTimer = 0;
+        }
+
+        stopLabel.text = "" + stopTimer.ToString("0");//小数点以下は切り捨て
+
+        if (interval % 60 == 0 && stopTimer <= 0)
         {
             GameObject enemyShell = Instantiate(enemyShellPrefab, transform.position, Quaternion.identity);
 
@@ -32,4 +48,15 @@ public class EnemyShotShell : MonoBehaviour
             Destroy(enemyShell, 3.0f);
         }
     }
+
+    // 敵の攻撃をストップさせるメソッド（Timerの時間を増加させることで攻撃の停止時間を伸ばす）
+    // （考え方）HPを増加させるアイテム等と同じ
+    // このアイテムを複数取得すると、それだけ攻撃停止時間も「加算」される。
+    public void AddStopTimer(float amount)
+    {
+        stopTimer += amount;
+
+        stopLabel.text = "" + stopTimer.ToString("0");
+    }
+
 }
---
+++ Assets/Scripts/StopAttackItem.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StopAttackItem : MonoBehaviour
+{
+    private GameObject[] targets;
+    [SerializeField]
+    private AudioClip getSound;
+    [SerializeField]
+    private GameObject effectPrefab;
+
+    void Update()
+    {
+        // 「EnemyShotShell」オブジェクトに「EnemyShotShell」タグを設定してください（ポイント）
+        targets = GameObject.FindGameObjectsWithTag("EnemyShotShell");
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(targets.Length);
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log(targets.Length);
+            for (int i = 0; i < targets.Length; i++)
+            {
+                // 攻撃停止時間を３秒加算する（何秒間加算するかは自由）
+                targets[i].GetComponent<EnemyShotShell>().AddStopTimer(3.0f);
+            }
+
+            Destroy(gameObject);
+            AudioSource.PlayClipAtPoint(getSound, transform.position);
+            GameObject effect = Instantiate(effectPrefab, transform.position, Quaternion.identity);
+            Destroy(effect, 0.5f);
+        }
+    }
+}
